76c1286e979a17184faa44bcd32577634a5fdf28
Joost S <joost.schermers@protonmail.com>
2023-11-11 20:49:40 +0100

    update component

--- index.ts
+++ index.ts
@@ -1,9 +1,33 @@
-interface Component {
-    test:string
+import { LitElement, html} from "lit";
+import {customElement, property} from 'lit/decorators.js';
+
+@customElement("donk-voetbalstanden")
+class DonkVoetbalstanden extends LitElement {
+
+    @property()
+    clientId?: string;
+    
+    private URL: string ="https://data.sportlink.com"
+    
+    private async getData(): Promise<void> {
+ 
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        this.getData();
+        this.dispatchEvent(new CustomEvent('connected'));
+      }
+    
+      render() {
+        return html`
+          <img src=${this.URL}/clublogo?client_id=${this.clientId} alt="logo"/>
+        `;
+      }
 }
 
-const objectJoost: Component = {
-    test: "test"
-}
-
-console.log(objectJoost);
+declare global {
+    interface HTMLElementTagNameMap {
+      'donk-voetbalstanden': DonkVoetbalstanden
+    }
+  }
